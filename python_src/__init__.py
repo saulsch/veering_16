bfd9f717642c94373db6609dc00b5cc45a9f375c
Saul Schleimer <saulsch@gmail.com>
2026-03-17 21:41:49 -0700

    added the try/except block to __init__.py

--- python_src/__init__.py
+++ python_src/__init__.py
@@ -1,7 +1,17 @@
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 def version():
     return __version__
 
 import sys
 from .database import get_tables, manifolds_path, original_manifolds_path
+
+try:
+    import snappy
+    table_dict = snappy.database.add_tables_from_package('veering_16', False)
+    for name, table in table_dict.items():
+        setattr(snappy, name, table)
+        if name not in snappy.database_objects:
+            snappy.database_objects.append(name)
+except ImportError:
+    raise RuntimeError('Error happened when loading veering 16 tet census data to SnapPy')
