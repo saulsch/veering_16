ef8b0a2225cda127a26035b5ff90d80f82464c59
Saul Schleimer <saulsch@gmail.com>
2026-03-20 18:16:13 -0700

    fixing the prefix

--- python_src/database.py
+++ python_src/database.py
@@ -32,7 +32,7 @@ def get_tables(ManifoldTable):
 
         # ISSUE: current names might overlap with decorated isosigs.
         # 2026-03-19: Address this by moving to the names vt[0-9]+
-        _regex = re.compile(r'(vt[0-9]+)$')
+        _regex = re.compile(r'(veer[0-9]+)$')
         _select = 'select name, triangulation, angles from %s '
 
         def __init__(self, **kwargs):
